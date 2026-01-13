d93a18786efc26ed29fdde4fed908ec3cbc74fba
mung96 <mung5079@gmail.com>
2026-01-13 09:36:44 +0900

    feat: done 상태 css

--- frontend/src/pages/todos/ui/todo-section/index.tsx
+++ frontend/src/pages/todos/ui/todo-section/index.tsx
@@ -3,7 +3,8 @@ import { TodoItem } from "@pages/todos/api/todo-get-list";
 import { todoQueries } from "@pages/todos/api/todo.queries";
 import { TodoCreateContainerButton } from "@pages/todos/ui/todo-create-container-button";
 import { TodoCreateIconButton } from "@pages/todos/ui/todo-create-icon-button";
-import { PenIcon, TrashIcon } from "@shared/ui/icons";
+import { cn } from "@shared/lib/style";
+import { CheckIcon, PenIcon, TrashIcon } from "@shared/ui/icons";
 import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 
 export const TodoSection = ({ onCreate }: { onCreate: () => void }) => {
@@ -23,28 +24,62 @@ export const TodoSection = ({ onCreate }: { onCreate: () => void }) => {
         </div>
         {itemCount > 0 ? <TodoCreateIconButton onCreate={onCreate} /> : null}
       </header>
-      <div
-        className="w-full rounded-2xl min-h-[207px] bg-gray-50 flex flex-col items-center justify-center gap-2"
-        style={{
-          backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='16' ry='16' stroke='%23D1D5DB' stroke-width='1' stroke-dasharray='8%2c 8' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e")`,
-        }}
-      >
+
+      <ul className="flex flex-col gap-2">
         {todoItems?.tasks?.map((todo) => (
-          <TodoItemComponent key={todo.taskId} todo={todo} />
+          <li key={todo.taskId}>
+            <TodoItemComponent todo={todo} isDone={true} />
+          </li>
         ))}
-        {itemCount === 0 && <TodoCreateContainerButton onCreate={onCreate} />}
-      </div>
+      </ul>
+      {itemCount === 0 && (
+        <div
+          className="w-full rounded-2xl min-h-[207px] bg-gray-50 flex flex-col items-center justify-center gap-2"
+          style={{
+            backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='16' ry='16' stroke='%23D1D5DB' stroke-width='1' stroke-dasharray='8%2c 8' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e")`,
+          }}
+        >
+          <TodoCreateContainerButton onCreate={onCreate} />
+        </div>
+      )}
     </section>
   );
 };
 
-export const TodoItemComponent = ({ todo }: { todo: TodoItem }) => {
+export const TodoItemComponent = ({
+  todo,
+  isDone = false,
+}: {
+  todo: TodoItem;
+  isDone?: boolean;
+}) => {
   return (
     <div
-      className="w-full h-11 rounded-2xl text-gray-950 pl-3 py-3 bg-gray-200 flex items-center justify-between"
+      className={cn(
+        "w-full h-11 rounded-2xl text-gray-950 py-3 bg-gray-200 flex items-center justify-between",
+        isDone && "bg-gray-100 opacity-50"
+      )}
       key={todo.taskId}
     >
-      <span className="text-body1 text-gray-950 font-regular">{todo.name}</span>
+      <div className="flex items-center">
+        <div className="w-11 h-11 flex items-center justify-center relative">
+          <div
+            className={cn(
+              "w-5 h-5 border border-gray-400 rounded-full bg-gray-100",
+              isDone && "bg-gray-600"
+            )}
+          />
+          {isDone && <CheckIcon className="absolute w-4 h-4 text-gray-0" />}
+        </div>
+        <span
+          className={cn(
+            "text-body1 text-gray-950 font-regular",
+            isDone && "line-through"
+          )}
+        >
+          {todo.name}
+        </span>
+      </div>
       <div className="flex items-center">
         <button className="w-11 h-11 flex items-center justify-center">
           <PenIcon className="w-4 h-4 text-gray-950" />
