4c64188a8bd3a334751e4d1a5d70efba6e68dcb8
mung96 <mung5079@gmail.com>
2026-01-22 09:59:21 +0900

    chore: env에서 next public 제거

--- frontend/src/shared/api/config.ts
+++ frontend/src/shared/api/config.ts
@@ -1,4 +1,4 @@
 
 
 export const INTERNAL_API_SERVER_URL ='/api/internal';
-export const API_SERVER_URL = process.env.NEXT_PUBLIC_API_SERVER_URL;
+export const API_SERVER_URL = process.env.API_SERVER_URL;
