084682ac4e9fac3d583e5c1e9a015618ff2a2d40
mung96 <mung5079@gmail.com>
2026-01-25 13:34:10 +0900

    chore: middleware 로그 추가

--- backend/src/middleware/visitor.middleware.ts
+++ backend/src/middleware/visitor.middleware.ts
@@ -18,10 +18,22 @@ export class VisitorMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
     const visitorId = req.headers['x-visitor-id'] as string;
 
+    console.log('[VisitorMiddleware] Request received:', {
+      method: req.method,
+      url: req.url,
+      visitorId: visitorId || 'not provided',
+    });
+
     if (visitorId) {
       const member =
         await this.memberService.findOrCreateByVisitorId(visitorId);
       req.member = member;
+      console.log('[VisitorMiddleware] Member found/created:', {
+        memberId: member.id,
+        visitorId: visitorId,
+      });
+    } else {
+      console.log('[VisitorMiddleware] No visitor ID provided');
     }
 
     next();
