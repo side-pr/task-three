60238c38ae3ba6e8c856451a627688e933e75572
mung96 <mung5079@gmail.com>
2026-01-28 20:53:19 +0900

    feat: react-native app-in-toss

---
+++ mobile/granite.config.ts
@@ -0,0 +1,17 @@
+import { appsInToss } from '@apps-in-toss/framework/plugins';
+import { defineConfig } from '@granite-js/react-native/config';
+
+export default defineConfig({
+  appName: 'task-three',
+  scheme: 'intoss',
+  plugins: [
+    appsInToss({
+      brand: {
+        displayName: 'task-three', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
+        primaryColor: '#3182F6', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
+        icon: '', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+      },
+      permissions: [],
+    }),
+  ],
+});
