b445f3c1495e787904c19724ff45471bdbb0a541
mung96 <mung5079@gmail.com>
2026-02-03 19:08:19 +0900

    fix: 로고 변경

--- mobile/granite.config.ts
+++ mobile/granite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       brand: {
         displayName: '3개할일', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
         primaryColor: '#3182F6', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-        icon: 'https://task-three-nine.vercel.app/icons/check_icon.svg', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+        icon: 'https://task-three-nine.vercel.app/logo/three-logo.png', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
       },
       permissions: [],
     }),
